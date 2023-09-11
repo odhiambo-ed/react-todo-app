3a8a47a1adac3b279faf8b15027c248194d097fd
odhiambo-ed <odhiambo.edward1112gmail.com>
2023-09-11 20:28:45 +0300

    add flex to container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className='container'>
-      <TodoForm />
+      <div className="d-flex flex-column justify-content-center align-items-center gap-3">
+        <TodoForm />
+        <TodoList />
+      </div>
     </div>
   );
 }
