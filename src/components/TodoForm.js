dfd790cdf550605790987b44ba1a7f27246aa9cf
odhiambo-ed <odhiambo.edward1112gmail.com>
2023-09-11 19:43:44 +0300

    add onchange function

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Form } from 'react-bootstrap'
 
 const TodoForm = () => {
-  return (
+    const [task setTask] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (task.trim() !== "") {
+            addTodo(task);
+            
+        }
+    }
+    return (
       <>
           <Form className='d-flex gap-2 justify-content-center'>
               <Form.Group controlId='taskInput'>
                   <Form.Control
-                      className='w-100'
-                      type='text'
-                      placeholder='Add a new task'
+                        className='w-100'
+                        type='text'
+                        placeholder='Add a new task'
+                        value={task}
+                        onChange={(e) => setTask(e.target.value)}
                   />
               </Form.Group>
-              <Button type='submit' variant='primary'>Add Task</Button>
+              <Button type='submit' variant='primary' onSubmit={handleSubmit}>Add Task</Button>
         </Form>
       </>
   )
