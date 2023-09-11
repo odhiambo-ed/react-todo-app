4447f605341c264de109484af63f13b0b77295bd
odhiambo-ed <odhiambo.edward1112gmail.com>
2023-09-11 20:16:48 +0300

    add bootstrap form validation

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -3,27 +3,40 @@ import { Button, Form } from 'react-bootstrap'
 
 const TodoForm = ({addTodo}) => {
     const [task, setTask] = useState("");
+    const [isValid, setIsvalid] = useState(true)
 
     const handleSubmit = (e) => {
         e.preventDefault();
         if (task.trim() !== "") {
             addTodo(task);
             setTask("");
+            setIsvalid(true)
+        } else {
+            setIsvalid(false)
         }
     }
     return (
       <>
-          <Form className='d-flex gap-2 justify-content-center'>
+            <Form className='d-flex gap-2 justify-content-center' onSubmit={handleSubmit}>
               <Form.Group controlId='taskInput'>
                   <Form.Control
                         className='w-100'
                         type='text'
                         placeholder='Add a new task'
                         value={task}
-                        onChange={(e) => setTask(e.target.value)}
-                  />
+                        onChange={(e) => {
+                            setTask(e.target.value);
+                            setIsvalid(true);
+                        }}
+                        isInvalid={!isValid}
+                    />
+                    {!isValid && (
+                        <Form.Control.Feedback type='invalid' className='text-danger'>
+                            Please enter valid task.
+                        </Form.Control.Feedback>
+                    )}
               </Form.Group>
-              <Button type='submit' variant='primary' onSubmit={handleSubmit}>Add Task</Button>
+              <Button type='submit' variant='primary'>Add Task</Button>
         </Form>
       </>
   )
