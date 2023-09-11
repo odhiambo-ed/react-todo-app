21babcb01f2add6ff92f72900bb22222a213fa3b
odhiambo-ed <odhiambo.edward1112gmail.com>
2023-09-11 20:45:16 +0300

    add an ADDTODOS function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { useState } from 'react';
 import './App.css';
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
 
 function App() {
+  const [todos, setTodos] = useState("")
+
+  const addTodos = (job) => {
+    setTodos([...todos], job)
+  }
   return (
     <div className='container'>
       <div className="d-flex flex-column justify-content-center align-items-center gap-3">
